4839540bc230e29f6343df95473865d3d5e78efe
Sean C. Hsu <sean_c_hsu@trendmicro.com>
2021-06-04 02:27:29 +0800

    feat: web query from db

--- cmd/web/web.go
+++ cmd/web/web.go
@@ -3,69 +3,43 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/ethereum/go-ethereum/common"
-	ethTypes "github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/eth-go-test/pkg/models"
 	"github.com/gin-gonic/gin"
-	"log"
-	"math/big"
 	"net/http"
 	"strconv"
 )
 
-var EthClient *ethclient.Client
-
-const (
-	EtheUrl = "https://data-seed-prebsc-2-s3.binance.org:8545/"
+var (
+	DB *models.DB
 )
 
-func addBlock(block *ethTypes.Block, result *[]gin.H) {
-	*result = append(*result, gin.H{
-		"block_num":   block.Number(),
-		"block_hash":  block.Hash(),
-		"block_time":  block.Time(),
-		"parent_hash": block.ParentHash(),
-	})
-}
-
 func getBlocks(c *gin.Context) {
-	var limit, i, defaultLimit uint32
+	var limit, defaultLimit uint64
 	var result []gin.H
 
 	defaultLimit = 10
 	limitString := c.Query("limit")
 	if limitString != "" {
-		l, err := strconv.ParseUint(limitString, 10, 32)
+		l, err := strconv.ParseUint(limitString, 10, 64)
 		if err != nil {
 			fmt.Printf("Parse limit err: %s", err)
 			c.String(http.StatusBadRequest, fmt.Sprintf("error: %s", err))
 		}
-		limit = uint32(l)
+		limit = l
 	} else {
 		limit = defaultLimit
 	}
 	ctx := context.Background()
+	// TODO: error handling
+	blocks := DB.GetBlocks(ctx, int(limit))
 
-	header, err := EthClient.HeaderByNumber(ctx, nil)
-	if err != nil {
-		fmt.Printf("Get header err: %s", err)
-		c.String(http.StatusInternalServerError, fmt.Sprintf("error: %s", err))
-	}
-
-	block, err := EthClient.BlockByNumber(ctx, header.Number)
-	if err != nil {
-		fmt.Printf("Get block err: %s", err)
-		c.String(http.StatusInternalServerError, fmt.Sprintf("error: %s", err))
-	}
-
-	addBlock(block, &result)
-	for i = 1; i < limit; i++ {
-		block, err := EthClient.BlockByHash(ctx, block.ParentHash())
-		if err != nil {
-			fmt.Printf("Get block err: %s", err)
-			c.String(http.StatusInternalServerError, fmt.Sprintf("error: %s", err))
-		}
-		addBlock(block, &result)
+	for _, block := range blocks {
+		result = append(result, gin.H{
+			"block_num":   block.BlockNum,
+			"block_hash":  block.BlockHash,
+			"block_time":  block.BlockTime,
+			"parent_hash": block.ParentHash,
+		})
 	}
 	c.JSON(200, gin.H{
 		"blocks": result,
@@ -73,73 +47,50 @@ func getBlocks(c *gin.Context) {
 }
 
 func getBlockByID(c *gin.Context) {
-	var transactions []common.Hash
-	id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+	var txs []string
+	id, _ := strconv.ParseUint(c.Param("id"), 10, 64)
 	ctx := context.Background()
-	block, err := EthClient.BlockByNumber(ctx, big.NewInt(id))
-	if err != nil {
-		fmt.Printf("Get block err: %s", err)
-		c.String(http.StatusInternalServerError, fmt.Sprintf("error: %s", err))
-	}
-	for _, tx := range block.Transactions() {
-		transactions = append(transactions, tx.Hash())
+	// TODO: error handling
+	block := DB.GetBlockByID(ctx, id)
+	for _, tx := range block.Transactions {
+		txs = append(txs, tx.TransactionHash)
 	}
 	c.JSON(200, gin.H{
-		"block_num":    block.Number(),
-		"block_hash":   block.Hash(),
-		"block_time":   block.Time(),
-		"parent_hash":  block.ParentHash(),
-		"transactions": transactions,
+		"block_num":    block.BlockNum,
+		"block_hash":   block.BlockHash,
+		"block_time":   block.BlockTime,
+		"parent_hash":  block.ParentHash,
+		"transactions": txs,
 	})
 }
 
 func getTxByHash(c *gin.Context) {
-	var from common.Address
+	var logs []gin.H
 	hash := c.Param("txHash")
 	ctx := context.Background()
-	// TODO: enhance error handling, and pending
-	tx, _, err := EthClient.TransactionByHash(ctx, common.HexToHash(hash))
-	if err != nil {
-		fmt.Printf("Get block err: %s", err)
-		c.String(http.StatusInternalServerError, fmt.Sprintf("error: %s", err))
+	// TODO: add error handling
+	tx := DB.GetTxByHash(ctx, hash)
+	for _, log := range tx.Logs {
+		logs = append(logs, gin.H{
+			"index": log.Index,
+			"data":  log.Data,
+		})
 	}
-	// TODO: enhance error handling
-	chainID, err := EthClient.NetworkID(ctx)
-	if err != nil {
-		fmt.Printf("Get chainID err: %s", err)
-		c.String(http.StatusInternalServerError, fmt.Sprintf("error: %s", err))
-	}
-	// TODO: enhance error handling
-	if msg, err := tx.AsMessage(ethTypes.NewEIP155Signer(chainID), tx.FeeCap()); err == nil {
-		from = msg.From()
-	}
-	// TODO: enhance error handling
-	receipt, err := EthClient.TransactionReceipt(ctx, tx.Hash())
-	if err != nil {
-		fmt.Printf("Get receipt err: %s", err)
-		c.String(http.StatusInternalServerError, fmt.Sprintf("error: %s", err))
-	}
+
 	c.JSON(200, gin.H{
-		"tx_hash": tx.Hash(),
-		"to":      tx.To(),
-		"from":    from,
-		"nonce":   tx.Nonce(),
-		"data":    tx.Data(),
-		"value":   tx.Value(),
-		"logs":    receipt.Logs,
+		"tx_hash": tx.TransactionHash,
+		"to":      tx.To,
+		"from":    tx.From,
+		"nonce":   tx.Nonce,
+		"data":    tx.Data,
+		"value":   tx.Value,
+		"logs":    logs,
 	})
 }
 
-func initEthClient() {
-	client, err := ethclient.Dial(EtheUrl)
-	if err != nil {
-		log.Fatal("connection fail", err)
-	}
-	EthClient = client
-}
-
 func main() {
-	initEthClient()
+	dsn := "host=db user=postgres password=example dbname=db port=5432 sslmode=disable TimeZone=Asia/Taipei"
+	DB = models.InitDB(dsn, 20)
 	r := gin.Default()
 	r.GET("/blocks", getBlocks)
 	r.GET("/blocks/:id", getBlockByID)
