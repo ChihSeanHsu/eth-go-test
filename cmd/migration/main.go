9625cee7e90b48b15180ba7456d58e5adb7989c2
Sean C. Hsu <sean_c_hsu@trendmicro.com>
2021-06-04 02:25:22 +0800

    feat: db migration

---
+++ cmd/migration/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/eth-go-test/pkg/models"
+)
+
+func main() {
+
+	dsn := "host=db user=postgres password=example dbname=db port=5432 sslmode=disable TimeZone=Asia/Taipei"
+	db := models.InitDB(dsn, 1)
+
+	db.AutoMigrate(&models.Block{}, &models.Transaction{}, &models.Log{})
+}
